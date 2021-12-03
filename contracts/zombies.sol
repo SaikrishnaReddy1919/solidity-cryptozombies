06295611863a58ef54b385b0b50d2590df721255
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2021-12-03 07:15:07 +0530

    keccak256 and typecasting

--- contracts/zombies.sol
+++ contracts/zombies.sol
@@ -24,7 +24,15 @@ contract ZombieFactory {
     // This function will view some of our contract's variables but not modify them, so mark it as view.
     // The function body should be empty at this point — we'll fill it in later.
     function _generateRandomDna(string memory _str) private view returns (uint) {
-        //
+
+        // The first line of code should take the keccak256 hash of abi.encodePacked(_str) to
+        // generate a pseudo-random hexadecimal, typecast it as a uint, and finally store
+        // the result in a uint called rand.
+        // We want our DNA to only be 16 digits long (remember our dnaModulus?).
+        // So the second line of code should return the above value modulus (%) dnaModulus.
+
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
     }
 
 }
