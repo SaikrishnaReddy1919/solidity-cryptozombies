98fe1f16f6a171e3bcd13a3fc80d6874ccaf6ebd
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2021-12-07 06:41:24 +0530

    storage - memory

--- contracts/zombieFeeding.sol
+++ contracts/zombieFeeding.sol
@@ -3,4 +3,10 @@ import "./zombieFactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
 
+    // When a zombie feeds on some other lifeform, its DNA will combine
+    // with the other lifeform's DNA to create a new zombie.
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public view {
+      require(msg.sender == zombieToOwner[_zombieId], "Sender must to equal to owner of this zombie");
+      Zombie storage myZombie = zombies[_zombieId];
+  }
 }
