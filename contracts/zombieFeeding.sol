b9a8fcc3a437e0df9023bb5662397f649211631d
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2021-12-07 07:06:18 +0530

    interacting wit other contracts : interface

--- contracts/zombieFeeding.sol
+++ contracts/zombieFeeding.sol
@@ -1,6 +1,21 @@
 pragma solidity >=0.5.0 <0.6.0;
 import "./zombieFactory.sol";
 
+contract KittyInterface {
+    function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+);
+}
+
 contract ZombieFeeding is ZombieFactory {
 
     // When a zombie feeds on some other lifeform, its DNA will combine
