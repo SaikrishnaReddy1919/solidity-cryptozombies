2b1b2119b28e78986a8738379f73a547a0f5943d
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2021-12-12 22:50:32 +0530

    zombie attack, time travel-ex(not availeble on mainnet-haha

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -1,6 +1,8 @@
 const CryptoZombies = artifacts.require("CryptoZombies");
 const utils = require("./helpers/utils");
+const time = require("./helpers/time");
 const zombieNames = ["Zombie 1", "Zombie 2"];
+
 contract("CryptoZombies", (accounts) => {
   let [alice, bob] = accounts;
   let contractInstance;
@@ -35,8 +37,24 @@ contract("CryptoZombies", (accounts) => {
       const newOwner = await contractInstance.ownerOf(zombieId);
       assert.equal(newOwner, bob);
     });
-    xit("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
-      // TODO: Test the two-step scenario.  The owner calls transferFrom
+    // The owner calls transferFrom
+    it("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
+      const result = await contractInstance.createRandomZombie(zombieNames[0], { from: alice });
+      const zombieId = result.logs[0].args.zombieId.toNumber();
+      await contractInstance.approve(bob, zombieId, { from: alice });
+      await contractInstance.transferFrom(alice, bob, zombieId, { from: alice });
+      const newOwner = await contractInstance.ownerOf(zombieId);
+      assert.equal(newOwner, bob);
     });
+  });
+  it("zombies should be able to attack another zombie", async () => {
+    let result;
+    result = await contractInstance.createRandomZombie(zombieNames[0], { from: alice });
+    const firstZombieId = result.logs[0].args.zombieId.toNumber();
+    result = await contractInstance.createRandomZombie(zombieNames[1], { from: bob });
+    const secondZombieId = result.logs[0].args.zombieId.toNumber();
+    await time.increase(time.duration.days(1));
+    await contractInstance.attack(firstZombieId, secondZombieId, { from: alice });
+    assert.equal(result.receipt.status, true);
   });
 });
