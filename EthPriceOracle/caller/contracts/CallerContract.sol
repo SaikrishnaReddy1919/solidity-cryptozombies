aa2e4b26946e8c3f54181bc517b1c3a89bff8733
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2021-12-14 05:18:01 +0530

    udpateEthPrice function from caller contract

--- EthPriceOracle/caller/contracts/CallerContract.sol
+++ EthPriceOracle/caller/contracts/CallerContract.sol
@@ -1,10 +1,19 @@
 pragma solidity >=0.5.0 <0.6.0;
 import "./EthPriceOracleInterface.sol";
-contract CallerContract {
-  EthPriceOracleInterface private oracleInstance;
-  address private oracleAddress;
-  function setOracleInstanceAddress (address _oracleInstanceAddress) public {
-    oracleAddress = _oracleInstanceAddress;
-    oracleInstance = EthPriceOracleInterface(oracleAddress);
-  }
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+contract CallerContract is Ownable { // 2. Make the contract inherit from `Ownable`
+    EthPriceOracleInterface private oracleInstance;
+    address private oracleAddress;
+    event newOracleAddressEvent(address oracleAddress);
+    function setOracleInstanceAddress (address _oracleInstanceAddress) public onlyOwner{
+      oracleAddress = _oracleInstanceAddress;
+      oracleInstance = EthPriceOracleInterface(oracleAddress);
+      emit newOracleAddressEvent(oracleAddress);
+    }
+
+    function updateEthPrice() public {
+        uint256 id = oracleInstance.getLatestEthPrice();
+        myRequests[id] = true;
+        emit ReceivedNewRequestIdEvent(id);
+    }
 }
