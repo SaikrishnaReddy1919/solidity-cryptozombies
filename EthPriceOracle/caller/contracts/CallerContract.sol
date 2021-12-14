4cc852adb95a2298cfd893ef25ace5d6f1f14553
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2021-12-14 05:41:29 +0530

    getting latest eth price logic, rand no generation

--- EthPriceOracle/caller/contracts/CallerContract.sol
+++ EthPriceOracle/caller/contracts/CallerContract.sol
@@ -20,11 +20,17 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
         emit ReceivedNewRequestIdEvent(id);
       }
     //   this function will get called after getting latest eth price from binance api
-      function callback(uint256 _ethPrice, uint256 _id) public {
+      function callback(uint256 _ethPrice, uint256 _id) public onlyOracle{
         require(myRequests[_id], "This request is not in my pending list.");
         ethPrice = _ethPrice;
         delete myRequests[_id];
         emit PriceUpdatedEvent(_ethPrice, _id);
       }
+
+      modifier onlyOracle() {
+      // Start here
+      require(msg.sender == oracleAddress, "You are not authorized to call this function.");
+      _;
+    }
   }
 
