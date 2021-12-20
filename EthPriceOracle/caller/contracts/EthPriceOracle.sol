88a90fc81b9160a9873b6c2a8e6d0a46065b0d46
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2021-12-20 21:21:09 +0530

    openzipplin Roles library

--- EthPriceOracle/caller/contracts/EthPriceOracle.sol
+++ EthPriceOracle/caller/contracts/EthPriceOracle.sol
@@ -1,28 +1,28 @@
 pragma solidity >=0.5.0 <0.6.0;
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/access/Roles.sol";
 import "./CallerContractInterface.sol";
-contract EthPriceOracle is Ownable {
+contract EthPriceOracle { 
+  using Roles for Roles.Role;
+  Roles.Role private owners;
+  Roles.Role private oracles;
   uint private randNonce = 0;
   uint private modulus = 1000;
   mapping(uint256=>bool) pendingRequests;
   event GetLatestEthPriceEvent(address callerAddress, uint id);
   event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
-  // Start here
   function getLatestEthPrice() public returns (uint256) {
-      randNonce++;
-      uint id = uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % modulus;
-
-      pendingRequests[id] = true;
-      emit GetLatestEthPriceEvent(msg.sender, id);
-      return id;
+    randNonce++;
+    uint id = uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % modulus;
+    pendingRequests[id] = true;
+    emit GetLatestEthPriceEvent(msg.sender, id);
+    return id;
   }
-
   function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public onlyOwner {
-      require(pendingRequests[_id], "This request is not in my pending list.");
-      delete pendingRequests[_id];
-      CallerContractInterface callerContractInstance;
-      callerContractInstance = CallerContractInterface(_callerAddress);
-      callerContractInstance.callback(_ethPrice, _id);
-      emit SetLatestEthPriceEvent(_ethPrice, _callerAddress);
+    require(pendingRequests[_id], "This request is not in my pending list.");
+    delete pendingRequests[_id];
+    CallerContractInterface callerContractInstance;
+    callerContractInstance = CallerContractInterface(_callerAddress);
+    callerContractInstance.callback(_ethPrice, _id);
+    emit SetLatestEthPriceEvent(_ethPrice, _callerAddress);
   }
 }
