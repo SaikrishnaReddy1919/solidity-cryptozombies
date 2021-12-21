d76d8d8d3163793caa28872f7707af3262d49e31
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2021-12-21 21:45:21 +0530

    check if address is already on oracle, emit

--- EthPriceOracle/caller/contracts/EthPriceOracle.sol
+++ EthPriceOracle/caller/contracts/EthPriceOracle.sol
@@ -16,6 +16,9 @@ contract EthPriceOracle {
   }
   function addOracle(address _oracle) public {
     require(owners.has(msg.sender), "Not an owner!");
+    require(!oracles.has(_oracle), "Already an oracle!");
+    oracles.add(_oracle);
+    emit AddOracleEvent(_oracle);
   }
   function getLatestEthPrice() public returns (uint256) {
     randNonce++;
